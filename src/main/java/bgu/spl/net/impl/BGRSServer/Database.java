ab2cd7b3448a6fde3de99d55447ed7e2924d627e
spl211 <baroz.omri@gmail.com>
2021-01-08 09:49:28 +0200

    8.1 comments

--- src/main/java/bgu/spl/net/impl/BGRSServer/Database.java
+++ src/main/java/bgu/spl/net/impl/BGRSServer/Database.java
@@ -43,12 +43,12 @@ public class Database {
     boolean initialize(String coursesFilePath) {
         coursesMap = new ConcurrentHashMap<>();
         File courseFile = new File(coursesFilePath);
-        int courseSerialNumber = 0;
+        int courseSerialNumber = 0;     //indicates the order in the file
         try (Scanner myScanner = new Scanner(courseFile)){
             while(myScanner.hasNextLine()){
-                String currCourse = myScanner.nextLine();
-                Course curr = new Course(currCourse, courseSerialNumber);
-                coursesMap.putIfAbsent(curr.getCourseNum(),curr);
+                String currCourse = myScanner.nextLine();   //get the string of the current course
+                Course curr = new Course(currCourse, courseSerialNumber);   //creates a course with the string and the serial number
+                coursesMap.putIfAbsent(curr.getCourseNum(),curr);       //put the new course in courseMap
                 courseSerialNumber++;
             }
         }catch (IOException e){
@@ -60,78 +60,140 @@ public class Database {
         return true;
     }
 
+    /**
+     * checks if a given password and username is the actual password of the username
+     * @param user  to check its password
+     * @param password  the password to compare
+     * @return  true if the passwords is equals
+     */
     public boolean checkMatch(String user, String password){
         String toCompare = registeredUserMap.get(user).getPassword();
         return toCompare.equals(password);
     }
 
+    /**
+     * chcks if a given username is registered to the server
+     * @param username  the username to check
+     * @return true if registered
+     */
     public boolean isRegistered(String username){
         return registeredUserMap.containsKey(username);
 
     }
 
+    /**
+     * chcks if a given username is logged in to the server
+     * @param username  the username to check
+     * @return true if logged in
+     */
     public boolean isLoggedIn(String username){
         return loggedInUserMap.containsKey(username);
     }
 
+    /**
+     * registers a user to the server
+     * @param user user to register
+     * @return  true if succeeds
+     */
     public boolean register(User user){
         User user1 = registeredUserMap.putIfAbsent(user.getUsername(),user);
+        //if user1 is null the it succeeded, otherwise there is already a user with the given user.username
         return user1 == null;
     }
 
+    /**
+     * get a user that is registered to the server by his username
+     * @param username the username of the user
+     * @return the User if such exists, otherwise null
+     */
     public User getUserByName(String username){
         return registeredUserMap.get(username);
     }
 
-    public void login(User user){
-        loggedInUserMap.putIfAbsent(user.getUsername(),user);
+    /**
+     * logs in a user to the server
+     * @param user  the user to login
+     * @return true if succeeded
+     */
+    public boolean login(User user){
+        User user1 = loggedInUserMap.putIfAbsent(user.getUsername(),user);
+        return user1 == null;
     }
 
+    /**
+     * logs out a user from the server
+     * @param user the user to logout
+     */
     public void logout(User user){
         loggedInUserMap.remove(user.getUsername());
     }
 
+    /**
+     * gets a course by his number
+     * @param courseNumber the coursenumber of the course to get
+     * @return  the course with the relevant courseNumber. if no such exists returns null
+     */
     public Course getCoursByNum(Short courseNumber){
         return coursesMap.get(courseNumber);
     }
 
+    /**
+     * checks if a user can register to a course, aka is registered to all of his kdamCourses already
+     * @param user  the user to check
+     * @param course    the course to check its kdamCourses
+     * @return true if the user can register to it, otherwise false
+     */
     public boolean checkKdam(User user, Course course){
-        List<Course> userCourses = user.getCourseList();
-        List<Short> userCoursesNumbers = new LinkedList<>();
+        List<Course> userCourses = user.getCourseList();    //gets the user's courses list
+        List<Short> userCoursesNumbers = new LinkedList<>();    //get all the courseNumbers of the user's courses
         for (Course course1 : userCourses){
             userCoursesNumbers.add(course1.getCourseNum());
         }
-        for (Short curr:course.getKdamCoursesList()) {
-            if (!userCoursesNumbers.contains(curr))
+        for (Short curr:course.getKdamCoursesList()) {  //goes through all the courses in the course kdam courses list
+            if (!userCoursesNumbers.contains(curr)) //if there is a course in the courses kdam course list, that the
+                                                    //user isn't registered to, return false
                 return false;
         }
         return true;
     }
 
-
+    /**
+     * registers a user to a course
+     * @param user the user to register
+     * @param courseNumber the course number of the course
+     * @return  true if succeeded, otherwise false
+     */
     public boolean registerToCourse(User user, Short courseNumber){
         Course curr = coursesMap.get(courseNumber);
+        //if there isn't such course, or the user doesn't have all the kdam courses, or there isn't any place left return false
         if (curr==null || !checkKdam(user,curr) || curr.getNumOfMaxStudents() == curr.getNumOfRegisteredStudents())
             return false;
         else{
+            //sycnchronized becouse several clients can register at the same time and then there would be too much
+            //students in the course
             synchronized (curr){
+                //double check for the place in the course
                 if (curr.getNumOfMaxStudents() > curr.getNumOfRegisteredStudents())
-                    return curr.registerUser(user);
+                    return curr.registerUser(user); //try to register the user to the course
             }
         }
-        return false;   //no course with courseNumber or
-                        // not enough place in course or
-                        // dont have all the kdam courses.
+        return false;
     }
 
+    /**
+     * unregisters a user from a course
+     * @param user the user no unregister
+     * @param courseNumber the course number to unregister from
+     * @return true if succeeds, otherwise false
+     */
     public boolean unregisterFromCourse(User user, Short courseNumber){
         Course curr = coursesMap.get(courseNumber);
+        //checks if there is such course
         if (curr!=null) {
             synchronized (curr) {
-                return curr.unregisterUser(user);
+                return curr.unregisterUser(user);   //try to unregister the user from the course
             }
         }
         return false;   //no course with courseNumber or
-                        //user wasn't registered to the course
     }
 }
--- src/main/java/bgu/spl/net/impl/BGRSServer/MessagingProtocolimpl.java
+++ src/main/java/bgu/spl/net/impl/BGRSServer/MessagingProtocolimpl.java
@@ -33,7 +33,7 @@ public class MessagingProtocolimpl implements MessagingProtocol<Message> {
     @Override
     public Message process(Message msg) {
         Short OP_CODE = msg.getOP_CODE();
-        //(hasn't logged in or not loged in) and it isn't ADMINREG, STUDENTREG or LOGIN OP_CODES
+        //(hasn't logged) and it isn't ADMINREG, STUDENTREG or LOGIN OP_CODES
         if (((user==null || !database.isLoggedIn(user.getUsername())) & OP_CODE>3))
             return new Message(ERROR,OP_CODE);
         Message output = null;
@@ -69,116 +69,203 @@ public class MessagingProtocolimpl implements MessagingProtocol<Message> {
         return shouldTerminate;
     }
 
+    /**
+     * a function that is used by admingReg and studentReg. It registers a user to the database
+     * @param props the array with the username (@props[0]) and password (@props[1]) of the user
+     * @param COMMAND   if it is ADMINGREG or STUDENTREG
+     * @param authorization true for adming and false for student
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message register(String[] props,Short COMMAND,boolean authorization){
         if (this.user != null |props.length<2 || database.isRegistered(props[0]))
-            return new Message(ERROR,COMMAND);     //not enough arguments or already registered
-        User user = new User(props[0],props[1],authorization);
-        if (database.register(user))
-            return new Message(ACK, COMMAND);      //succeeded
+            return new Message(ERROR,COMMAND);     //client is already logged in or not enough arguments or already registered
+        User user = new User(props[0],props[1],authorization);  //creates new user to register to the database
+        if (database.register(user))    //succeeded in registering to the database
+            return new Message(ACK, COMMAND);
         else
-            return new Message(ERROR,COMMAND);     //not enough arguments or already registered
+            return new Message(ERROR,COMMAND);     //failed in registering to the database
     }
 
+    /**
+     * registering an admin to the database
+     * @param props the array with the username (@props[0]) and password (@props[1]) of the admin
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message admingReg(String[] props){
         return register(props,ADMINREG,true);
     }
 
+    /**
+     * registering a student to the database
+     * @param props the array with the username (@props[0]) and password (@props[1]) of the student
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message studentReg(String[] props){
         return register(props,STUDENTREG,false);
     }
 
+    /**
+     * logs in a user to the database
+     * @param props the array with the username (@props[0]) and password (@props[1]) of the student
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message login(String[] props){
-        if (props.length<2 || (user==null & !database.isRegistered(props[0])) || !database.checkMatch(props[0],props[1]) || database.isLoggedIn(props[0]))
-            return new Message(ERROR,LOGIN);    // not enough arguments, or not registered, or password doesnt match or already logged in
+        //check if there aren't enough arguments, or user isn't registered, or the given password doesn't match
+        // to the user's password or user is already logged in
+        if (props.length<2 || (user==null & !database.isRegistered(props[0])) ||
+                !database.checkMatch(props[0],props[1]) || database.isLoggedIn(props[0]))
+            return new Message(ERROR,LOGIN);
         if (user==null)
-            user = database.getUserByName(props[0]);
-        database.login(user);
-        return new Message(ACK,LOGIN);
+            user = database.getUserByName(props[0]);    //get the user with the propreate username from database
+        if (database.login(user))
+            return new Message(ACK,LOGIN);
+        return new Message(ERROR,LOGIN);
     }
 
+    /**
+     * logs out the client from the server, also terminates the protocol
+     * @return  an ACK message
+     */
     private Message logout(){
-        database.logout(user);
-        shouldTerminate = true;
+        database.logout(user);  //logout from database
+        shouldTerminate = true; //change shouldTerminate to true
         return new Message(ACK,LOGOUT);
     }
 
+    /**
+     * registers the client to a course
+     * @param courseNumber the course number to register to
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message courseReg(Short courseNumber){
+        //checks that the client isn't an admin and tries to register it to coursse
         if (!user.isAdmin() && database.registerToCourse(user, courseNumber))
             return new Message(ACK, COURSEREG);
         return new Message(ERROR,COURSEREG);
     }
 
+    /**
+     * check kdam courses of a given course
+     * @param courseNumber the course number to return its kdam courses
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message kdamCheck(Short courseNumber){
-        List<Course> kdamCourses = getKdamCoursesByOrder(courseNumber);
+        List<Course> kdamCourses = getKdamCoursesByOrder(courseNumber); //gets the kdam courses in order
+        if (kdamCourses==null)  //if it is null, no course with courseNumber exists and returns error
+            return new Message(ERROR,KDAMCHECK);
         String[] data = new String[kdamCourses.size()];
         int i = 0;
+        //put in data all the course numbers in order
         for (Course curr : kdamCourses){
             data[i] = Short.toString(curr.getCourseNum());
             i++;
         }
-        String[] output = {Arrays.toString(data).replace(", ",",")};
+        String[] output = {Arrays.toString(data).replace(", ",",")};    //delete all the spaces
         return new Message(ACK,output,KDAMCHECK);
     }
 
+    /**
+     * assistant function for kdamCheck. it returns all the kdam courses of a given course
+     * by order in the Courses.txt file.
+     * @param courseNumber the course number to return its kdam courses
+     * @return  null if no course with courseNumber exists, otherwise a list with all the kdamCourses sorted by
+     *          serial number
+     */
     private List<Course> getKdamCoursesByOrder(short courseNumber){
-        Course courseData = database.getCoursByNum(courseNumber);
-        List<Short> kdamCourseNumbers = courseData.getKdamCoursesList();
+        Course courseData = database.getCoursByNum(courseNumber);   //get the course from the database
+        if (courseData==null)   //null if no such course exists
+            return null;
+        List<Short> kdamCourseNumbers = courseData.getKdamCoursesList();    //get the kdam courses list by short
         List<Course> kdamCourses = new LinkedList<>();
+        //make a list of Course of the short value
         for (Short shrt : kdamCourseNumbers){
             kdamCourses.add(database.getCoursByNum(shrt));
         }
-        Comparator<Course> cmp = Comparator.comparingInt(Course::getSerialNumber);
-        kdamCourses.sort(cmp);
+        Comparator<Course> cmp = Comparator.comparingInt(Course::getSerialNumber); //comparator to compare courses
+                                                                                    //by serial number
+        kdamCourses.sort(cmp);  //sort the Courses list
         return kdamCourses;
     }
 
+    /**
+     *  an admin OP CODE, checks the status of a given course
+     * @param courseNumber the course number to checks its status
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message courseStatus(Short courseNumber){
-        Course courseData = database.getCoursByNum(courseNumber);
-        if (!user.isAdmin() | courseData==null) {
+        Course courseData = database.getCoursByNum(courseNumber);   //gets the course from database
+        if (!user.isAdmin() | courseData==null) {   //if the client isn't an admin or there isn't such course return ERROR
             return new Message(ERROR, COURSESTAT);
         }
         String[] data = {courseData.toString()};
         return new Message(ACK,data,COURSESTAT);
     }
 
+    /**
+     * an admin OP CODE, checks the status of a given student
+     * @param props holds the username at props[0]
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message studentStatus(String[] props){
-        if (!user.isAdmin() | props.length<1)
+        if (!user.isAdmin() | props.length<1)   //if the client isn't an admin or there aren't enough arguments return ERROR
             return new Message(ERROR,STUDENTSTAT);
-        User user = database.getUserByName(props[0]);
+        User user = database.getUserByName(props[0]);   //get the user to check its status
         String output = "Student: " + props[0] + "\n" + "Courses: " + getCoursesNumbersByOrder(user).replace(", ",",");
         String[] data = {output};
         return new Message(ACK,data,STUDENTSTAT);
     }
 
+    /**
+     * a student OP CODE, checks if the student is registered to a given course
+     * @param courseNumber the course number to check if the user is registered to
+     * @return "REGISTERED" if registered, "NOT REGISTERED" if not registered or ERROR message if fails
+     */
     private Message isRegistered(Short courseNumber){
-        Course course = database.getCoursByNum(courseNumber);
-        if (user.isAdmin() | course==null)
+        Course course = database.getCoursByNum(courseNumber);   // get the course from database
+        if (user.isAdmin() | course==null)  //if the user is an admin or there isn't such course return ERROR
             return new Message(ERROR,ISREGIDTERED);
-        if(course.getRegisteredStudents().contains(user)) {
-            String[] data = {"REGISTERED"};
+        if(course.getRegisteredStudents().contains(user)) { //if the user is registered
+            String[] data = {"REGISTERED"}; //return "REGISTERED"
             return new Message(ACK,data,ISREGIDTERED);
         }
-        String[] data = {"NOT REGISTERED"};
+        String[] data = {"NOT REGISTERED"}; //else return "NOT REGISTERED"
         return new Message(ACK,data,ISREGIDTERED);
     }
 
+    /**
+     * a student OP CODE, unregisters the student from a given course
+     * @param courseNumber the course number to unregister from
+     * @return  an ACK message if succedes and ERROR if fails
+     */
     private Message unregister(Short courseNumber){
+        //if the user isn't an admin and it succeeded in unregistering from the course return ACK
         if (!user.isAdmin() && database.unregisterFromCourse(user,courseNumber))
             return new Message(ACK,UNREGISTER);
         return new Message(ERROR,UNREGISTER);
     }
 
+    /**
+     * a student OP CODE
+     * @return a list of the courses the student is registered to by order in Courses.txt
+     */
     private Message myCourses(){
-        if (user.isAdmin())
+        if (user.isAdmin()) //if client is an admin return ERROR
             return new Message(ERROR,MYCOURSES);
         String[] data = {getCoursesNumbersByOrder(user).replace(", ",",")};
         return new Message(ACK,data,MYCOURSES);
     }
 
+    /**
+     *
+     * @param user to get his courses
+     * @return  a list of courses numbers that the user is registered to ordered by the place in Courses.txt,
+     *          returns as String
+     */
     private String getCoursesNumbersByOrder(User user) {
-        List<Course> courses = user.getRegisteredCoursesByOrder();
+        List<Course> courses = user.getRegisteredCoursesByOrder();  //get the courses by order
         short[] coursesNumbers = new short[courses.size()];
         int counter = 0;
+        //copy the courses numbers to coursesNumbers
         for (Course course : courses){
             coursesNumbers[counter] = course.getCourseNum();
             counter++;
